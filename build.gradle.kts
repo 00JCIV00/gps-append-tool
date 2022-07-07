c7421d92800d8a6012ef3931264af641ef90c491
00JCIV00 <68087632+00JCIV00@users.noreply.github.com>
2022-07-06 22:07:14 -0400

    Initial Setup
    - Setup basics for CLI parameter options, to include:
    -- filetype
    -- gpsdata
    -- input
    -- output

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.21"
+    kotlin("jvm") version "1.7.0"
     application
 }
 
 group = "org.jc__iv"
-version = "1.0-SNAPSHOT"
+version = "0.0.1a"
 
 repositories {
     mavenCentral()
@@ -14,6 +14,7 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("com.github.ajalt.clikt:clikt:3.5.0")
 }
 
 tasks.test {
@@ -21,9 +22,21 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "1.8"
 }
 
 application {
     mainClass.set("MainKt")
+}
+
+tasks.jar {
+    duplicatesStrategy = org.gradle.api.file.DuplicatesStrategy.INCLUDE
+
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+
+    configurations["compileClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,65 @@
-fun main(args: Array<String>) {
-	println("Hello World!")
+/*
+Author:     Jake Crawford
+Created:    06 July 2022
+Updated:    06 July 2022
+Version:	0.0.1a
 
-	// Try adding program arguments via Run/Debug configuration.
-	// Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-	println("Program arguments: ${args.joinToString()}")
+Details:	Append GPS data to survey files
+ */
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.validate
+
+
+class GAT: CliktCommand(help = "GPS Append Tool.") {
+	// Commands
+	val filetype_opt: String? by option("-f", "--filetype", help = "Specify the filetype of the input and output survey files. (Defaults to the filetype of the input file or '.pcap' failing that.)")
+
+	val gpsdata: String? by option("-g", "--gpsdata", help = "Specify the file containing GPS data.").
+		validate {require(it.isNotEmpty()){"--gpsdata file must be specified."} }
+
+	val input: String? by option("-i", "--input", help = "Specify the survey file to append GPS data onto.").
+		validate {require(it.isNotEmpty()){"--input file must be specified."} }
+
+	val output_opt: String? by option("-o", "--output", help = "Specify the name of the output file for the new survey file with appended GPS data. (Defaults to the input file name with '_gps' appended before the filetype.)")
+
+	lateinit var filetype: String
+
+	lateinit var output: String
+
+
+	override fun run() {
+		// Validation
+		if(input.isNullOrEmpty()) {
+			echo("--input file must be specified.")
+			return
+		}
+
+		if(gpsdata.isNullOrEmpty()) {
+			echo("--gpsdata file must be specified.")
+			return
+		}
+
+		val inputSplit = input!!.split('.')
+
+		if(filetype_opt.isNullOrEmpty() && inputSplit.size > 1) {
+			filetype = ".${inputSplit.last()}"
+		}
+		else filetype = ".pcap"
+
+		if(output_opt.isNullOrEmpty()) {
+			output = "${inputSplit.subList(0, inputSplit.lastIndex).joinToString(".")}_gps$filetype"
+		}
+
+		// Logic
+		for(item in listOf(input, gpsdata, filetype, output)){
+			echo(item)
+		}
+	}
+
+}
+
+fun main(args: Array<String>) {
+	GAT().main(args)
 }
